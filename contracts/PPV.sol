19f7550820b2287cc21a57f817c513df56431b00
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2021-04-30 22:06:17 +0530

    Comitted PPV.sol

--- contracts/PPV.sol
+++ contracts/PPV.sol
@@ -1,5 +1,19 @@
-pragma solidity 0.4.25;
+pragma solidity ^0.5.16;
 
 contract PPV {
-    
+
+    struct Video {
+        uint rentCount;
+    }
+
+    mapping(uint => Video) public videos;
+
+    uint public videosCount;
+
+    constructor () public {
+    }
+
+    function rentVideo (uint _vidId) public {
+        videos[_vidId].rentCount ++;
+    }
 }
---
+++ migrations/2_ppv_migration.js
@@ -0,0 +1,5 @@
+var PPV = artifacts.require("./PPV.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(PPV);
+};
